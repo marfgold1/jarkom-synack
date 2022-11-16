9fd2be7890b843803dd732d78e2e905bb884c83b
JeremyRio <jeremy.reinal@gmail.com>
2022-11-16 14:49:46 +0700

    feat: add Address class for ip & port, add Connection constructor, add send_data

--- lib/connection.py
+++ lib/connection.py
@@ -1,14 +1,29 @@
 import socket
 from .segment import Segment
+from typing import NamedTuple
+
+
+class Address(NamedTuple):
+    ip: str
+    port: int
+
 
 class Connection:
-    def __init__(self, ip : str, port : int):
+    def __init__(self, bind_port: int | None = None):
         # Init UDP socket
-        pass
+        self.socket = socket.socket(
+            socket.AF_INET,
+            socket.SOCK_DGRAM,
+            socket.IPPROTO_UDP,
+        )
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.socket.settimeout(0.1)
+        if bind_port:
+            self.socket.bind(Address(ip='', port=bind_port))
 
-    def send_data(self, msg : Segment, dest : ("ip", "port")):
+    def send_data(self, msg: Segment, dest: Address):
         # Send single segment into destination
-        pass
+        self.socket.sendto(msg.to_bytes(), dest)
 
     def listen_single_segment(self) -> Segment:
         # Listen single UDP datagram within timeout and convert into segment
