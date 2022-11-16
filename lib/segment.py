7d4d279a1aa9b06e2fbeb19a722358f50395e099
Fachry Dennis Heraldi <13520139@std.stei.itb.ac.id>
2022-11-16 11:30:59 +0700

    feat: add definition for class Segment, class SegmentFlag, and crc function

--- lib/segment.py
+++ lib/segment.py
@@ -1,73 +1,128 @@
 import struct
+from dataclasses import astuple, dataclass
+from typing import Type
 
-# Constants 
-SYN_FLAG = 0b0
-ACK_FLAG = 0b0
-FIN_FLAG = 0b0
+# Constants
+# ---------
+# The struct_format consist of following format:
+# ! for network endian (big-endian)
+# 2 ulong (2I) for 4 bytes each seq_num and ack_num
+# uchar (B) for 1 byte flags
+# pad byte (x) for 1 byte pad
+# ushort (H) for 2 bytes checksum
+# payload (s) for max 32756 bytes payload
+struct_format = '!2IBxH32756s'
+# The length of segment
+segment_length = 32768
+payload_length = 32756
+# bitmask for flag
+IDX_SYN = 1
+IDX_ACK = 4
+IDX_FIN = 0
+BIT_SYN = 1 << IDX_SYN
+BIT_ACK = 1 << IDX_ACK
+BIT_FIN = 1 << IDX_FIN
+# CRC-16/ARG
+# Polynom x^16 + x^15 + x^2 + 1
+# only LSB (0b1000000000000101)
+# repr in 0x8005, but reversed
+crc_poly = int(bin(0x8005)[:1:-1], 2)
 
-class SegmentFlag:
-    def __init__(self, flag : bytes):
-        # Init flag variable from flag byte
-        pass
+def _crc(data) -> int:
+    # Calculate CRC-16/ARC
+    res = 0
+    for byte in data:
+        res ^= byte
+        for _ in range(8):
+            if res & 1:
+                res = (res >> 1) ^ crc_poly
+            else:
+                res >>= 1
+    return res
 
-    def get_flag_bytes(self) -> bytes:
+
+@dataclass
+class SegmentFlag(object):
+    syn: bool = False
+    ack: bool = False
+    fin: bool = False
+
+    def test(self, syn=False, ack=False, fin=False) -> bool:
+        # Test this class
+        return (
+            self.syn == syn and
+            self.ack == ack and
+            self.fin == fin
+        )
+
+    @classmethod
+    def from_int(cls, flags: int):
+        # Convert byte form to this object
+        return cls(
+            syn=bool(flags & BIT_SYN),
+            ack=bool(flags & BIT_ACK),
+            fin=bool(flags & BIT_FIN),
+        )
+
+    def to_int(self) -> int:
         # Convert this object to flag in byte form
-        pass
+        return (
+            (int(self.syn) << IDX_SYN) |
+            (int(self.ack) << IDX_ACK) |
+            (int(self.fin) << IDX_FIN)
+        )
 
 
+@dataclass
+class Segment(object):
+    seq_num: int = 0
+    ack_num: int = 0
+    flags: SegmentFlag = SegmentFlag()
+    checksum: int = 0
+    payload: bytes = b''
 
+    def __post_init__(self):
+        if isinstance(self.flags, int):
+            self.flags = SegmentFlag.from_int(self.flags)
+        elif not isinstance(self.flags, SegmentFlag):
+            raise TypeError('flags must be SegmentFlag or int')
 
-
-class Segment:
-    # -- Internal Function --
-    def __init__(self):
-        # Initalize segment
-        pass
-
-    def __str__(self):
-        # Optional, override this method for easier print(segmentA)
-        output = ""
-        output += f"{'Sequence number':24} | {self.sequence}\n"
-        return output
-
-    def __calculate_checksum(self) -> int:
-        # Calculate checksum here, return checksum result
-        pass
-
-
-    # -- Setter --
-    def set_header(self, header : dict):
-        pass
-
-    def set_payload(self, payload : bytes):
-        pass
-
-    def set_flag(self, flag_list : list):
-        pass
-
-
-    # -- Getter --
-    def get_flag(self) -> SegmentFlag:
-        pass
-
-    def get_header(self) -> dict:
-        pass
-
-    def get_payload(self) -> bytes:
-        pass
-
+    def _calculate_checksum(self) -> int:
+        # Calculate checksum of this object using CRC-16/ARC
+        last_checksum = self.checksum
+        self.checksum = 0
+        segment_check = self.to_bytes(False)
+        # process segment_check and pass to result
+        res = _crc(segment_check)
+        self.checksum = last_checksum
+        return res
 
     # -- Marshalling --
-    def set_from_bytes(self, src : bytes):
-        # From pure bytes, unpack() and set into python variable
-        pass
-
-    def get_bytes(self) -> bytes:
+    def to_bytes(self, set_checksum=True) -> bytes:
         # Convert this object to pure bytes
-        pass
+        if set_checksum:
+            self.checksum = self._calculate_checksum()
+        self.flags = self.flags.to_int()
+        res = struct.pack(struct_format, *astuple(self))
+        self.flags = SegmentFlag.from_int(self.flags)
+        return res
 
+    @classmethod
+    def from_bytes(cls, src: bytes) -> Type['Segment'] | None:
+        # From pure bytes, unpack() and set into python variable
+        if src is not None:
+            return Segment(*struct.unpack(struct_format, src))
+        return None
 
     # -- Checksum --
     def valid_checksum(self) -> bool:
-        # Use __calculate_checksum() and check integrity of this object
-        pass
+        # Use _calculate_checksum() and check integrity of this object
+        last_checksum = self.checksum
+        self.checksum = 0
+        segment_check = self.to_bytes(False)
+        # we append reversed checksum
+        segment_check = segment_check + last_checksum.to_bytes(2, 'big')[::-1]
+        # compute modulo
+        res = _crc(segment_check)
+        self.checksum = last_checksum
+        return res == 0
