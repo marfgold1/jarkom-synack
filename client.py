8b0ac3424acb27f81684d39c920fe157afba00b5
Amar Fadil <34912365+marfgold1@users.noreply.github.com>
2022-11-17 19:17:03 +0700

    feat: add client and server

--- client.py
+++ client.py
@@ -1,22 +1,181 @@
-import lib.connection
-from lib.segment import Segment
-import lib.segment as segment
+import argparse
+from pathlib import Path
 
-class Client:
-    def __init__(self):
-        # Init client
-        pass
+from lib.connection import Address, Connection, handshake_timeout
+from lib.helper import DirectoryValidator, Metadata
+from lib.segment import Segment, SegmentFlag, payload_length, signature
+from logger import Logger
+
+
+class Client(object):
+    def __init__(
+        self,
+        path_output: Path,
+        client_port=None,
+        broadcast_port=1234,
+        broadcast_ip='255.255.255.255',
+    ):
+        self.path_output = path_output
+        self.conn = Connection(client_port)
+        self.broadcast_addr = Address(
+            ip=broadcast_ip,
+            port=broadcast_port,
+        )
+        self.conn.send_data(
+            Segment(flags=SegmentFlag(syn=True), payload=signature),
+            self.broadcast_addr,
+        )
+        Logger.log(
+            f'[!] [{self.broadcast_addr}] Sent broadcast signature.',
+            1,
+        )
+        ip, port = self.conn.socket.getsockname()
+        Logger.log(f'[!] [{ip}:{port}] Client started.', 3)
 
     def three_way_handshake(self):
         # Three Way Handshake, client-side
-        pass
+        Logger.log(
+            '[!] [Handshake] Waiting for server to initate connection',
+            3,
+        )
+        segment, addr = self.conn.listen_single_segment(None)
+        if segment.flags.test(syn=True):
+            Logger.log(
+                f'[!] [Handshake|SYN] [<{addr}] Receive SYN from server', 2,
+            )
+        Logger.log(
+            f'[!] [Handshake|SYN-ACK] [>{addr}] Sending SYN-ACK to the server',
+            2,
+        )
+        self.conn.send_data(
+            Segment(flags=SegmentFlag(syn=True, ack=True)),
+            addr,
+        )
+        Logger.log(
+            f'[!] [Handshake] [?{addr}] Waiting server response for ACK', 1,
+        )
+        data = self.conn.listen_single_segment(handshake_timeout, addr)
+        if not data:
+            Logger.log(
+                f'[!] [Handshake|ACK] [?{addr}] Server ACK response timeout.',
+                2,
+            )
+            return False
+        segment, addr = data
+        if segment.flags.test(ack=True):
+            Logger.log(
+                f'[!] [Handshake|ACK] [<{addr}] Receive ACK from server', 2,
+            )
+        return True
 
     def listen_file_transfer(self):
         # File transfer, client-side
-        pass
+        Logger.log('[!] [File Transfer] Waiting for file transfer...', 3)
+        seq_base = 0
+        segment, addr = self.conn.listen_single_segment(None)
+        if segment.flags.test(meta=True):
+            Logger.log([
+                f'[Segment META] [<{addr}] Received file metadata',
+                'from the server. Starting file transfer.',
+            ], 3)
+            file_meta = Metadata.from_bytes(segment.payload)
+        with open(self.path_output / file_meta.file_name, 'wb') as file:
+            last_payload = None
+            while True:
+                segment, addr = self.conn.listen_single_segment(None, addr)
+                if segment.flags.test(fin=True):
+                    self.conn.close_socket(addr)
+                    break
+                if not segment.valid_checksum():
+                    # invalid checksum, ignore.
+                    Logger.log([
+                        f'[Segment SEQ={segment.seq_num}] [<{addr}]',
+                        'Invalid checksum. Ignored.',
+                    ], 1)
+                    continue
+                if segment.seq_num < seq_base:
+                    # duplicate segment, try to send additional ack
+                    # prevent infinite loop for slow connection
+                    Logger.log([
+                        f'[Segment SEQ={segment.seq_num}] [<{addr}] Duplicate',
+                        'segment. Resending additional ACK.',
+                    ], 1)
+                    self.conn.send_data(
+                        Segment(
+                            flags=SegmentFlag(ack=True),
+                            ack_num=segment.seq_num,
+                        ),
+                        addr,
+                    )
+                    continue
+                if segment.seq_num == seq_base:
+                    if last_payload:
+                        file.write(last_payload)
+                    last_payload = segment.payload
+                    # Acknowledge it
+                    self.conn.send_data(
+                        Segment(
+                            flags=SegmentFlag(ack=True),
+                            ack_num=seq_base,
+                        ),
+                        addr,
+                    )
+                    Logger.log([
+                        f'[Segment SEQ={segment.seq_num}] [<{addr}]',
+                        'Received, Ack sent.',
+                    ], 2)
+                    seq_base += 1
+                    continue
+                Logger.log([
+                    f'[Segment SEQ={segment.seq_num}] [<{addr}] Received',
+                    'out-of-order segment. Ignored.',
+                ], 1)
+            file.write(last_payload[
+                :file_meta.file_size % payload_length or payload_length
+            ])
+        Logger.log(f'[!] [File Transfer] [{addr}] File transfer completed!', 3)
 
 
 if __name__ == '__main__':
-    main = Client()
-    main.three_way_handshake()
-    main.listen_file_transfer()
+    args_parser = argparse.ArgumentParser(
+        description='Client for simple TCP-like file transfer',
+    )
+    args_parser.add_argument(
+        'client_port',
+        type=int,
+        help='Client port. Set to 0 if you want to use random port.',
+    )
+    args_parser.add_argument(
+        'broadcast_port',
+        type=int,
+        help='Broadcast port. Default is 1234.',
+    )
+    args_parser.add_argument(
+        'path_output',
+        action=DirectoryValidator,
+        help='Path to output directory to store the file.',
+    )
+    args_parser.add_argument(
+        '-sh',
+        '--server_host',
+        default='255.255.255.255',
+        help='Server host address, if the server outside the subnet',
+    )
+    args_parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        help='Verbosity level.',
+    )
+    args = args_parser.parse_args()
+    default_port = 1234
+    main = Client(
+        args.path_output,
+        args.client_port if args.client_port else None,
+        args.broadcast_port if args.broadcast_port else default_port,
+        args.server_host,
+    )
+    Logger.level = args.verbose
+    if main.three_way_handshake():
+        main.listen_file_transfer()
--- server.py
+++ server.py
@@ -1,67 +1,225 @@
 import argparse
-from lib.connection import Address, Connection
-from lib.segment import Segment, SegmentFlag
+from concurrent.futures import ThreadPoolExecutor
+from io import BufferedReader
+from math import ceil
+from pathlib import Path
+
+from lib.connection import Address, Connection, handshake_timeout
+from lib.connection import signature_buffer as sign_buf
+from lib.helper import FileValidator, Metadata
+from lib.segment import Segment, SegmentFlag, payload_length
+from logger import Logger
+
+WINDOW_SIZE = 5
+
 
 class Server:
-    def __init__(self, path_file, port=1234):
+    def __init__(self, path_file: Path, port=1234, parallel=False):
         # Init server
         self.path_file = path_file
-        self.file_size = 0
-        self.conn = Connection(port)
+        self.conn = Connection(port, parallel)
         self.clients = []
+        self.parallel = parallel
         ip, port = self.conn.socket.getsockname()
-        print('[!] Server started at', f'{ip}:{port}')
-        pass
+        # Get file size
+        with open(self.path_file, 'rb') as file:
+            file.seek(0, 2)  # Seek to end
+            self.file_size = file.tell()
+        Logger.log(f'[!] [{ip}:{port}] Server started.', 3)
 
     def listen_for_clients(self):
         # Waiting client for connect
-        print('[!] Listening to broadcast address for clients.')
-        segment, addr = self.conn.listen_single_segment()
-        if segment.flags.test(syn=True):
-            print(
-                '[!] Received request from',
-                f'{addr.ip}:{addr.port}',
+        Logger.log('[!] Listening to broadcast address for clients.', 3)
+        executor = ThreadPoolExecutor()
+        while True:
+            segment, addr = self.conn.listen_single_segment(
+                timeout=None,
+                address=sign_buf if self.parallel else None,
             )
-            self.clients.append(addr)
-            return
+            if segment.is_signature():
+                Logger.log(f'[!] [<{addr}] Received signature request.', 3)
+                self.clients.append(addr)
+                if self.parallel:
+                    executor.submit(
+                        self.parallel_file_transfer,
+                        self.clients.pop(),
+                    )
+                else:
+                    if (input('[?] Listen more? ([y]/n) ') or 'y') != 'y':
+                        break
+            else:
+                Logger.log(f'[!] [<{addr}] Received unknown segment.', 1)
+        if not self.parallel:
+            Logger.log('[!] Commencing file transfer...', 3)
+            for client in self.clients:
+                self.file_transfer(client)
         else:
-            print(
-                '[!] Received unknown segment from',
-                f'{addr.ip}:{addr.port}',
-            )
+            executor.submit(self.parallel_file_transfer, self.clients.pop())
+        executor.shutdown(wait=True, cancel_futures=False)
 
-    def start_file_transfer(self):
-        # Handshake & file transfer for all client
-        pass
+    def parallel_file_transfer(self, client: Address):
+        # Parallel file transfer
+        Logger.log('[!] Commencing parallel file transfer...', 3)
+        self.file_transfer(client)
 
-    def file_transfer(self, client_addr: Address):
+    def __send_file_segment(
+        self,
+        file: BufferedReader,
+        seq: int,
+        client_addr: Address,
+    ):
+        # Send file segment to client
+        file.seek(seq * payload_length)
+        self.conn.send_data(
+            Segment(
+                seq_num=seq,
+                payload=file.read(payload_length),
+            ),
+            client_addr,
+        )
+        Logger.log(f'[Segment SEQ={seq}] [>{client_addr}] Sent.', 2)
+
+    def file_transfer(self, client_addr: Address, max_retry=3):
+        if not self.three_way_handshake(client_addr):
+            Logger.log([
+                f'[!] [File Transfer] [{client_addr}]',
+                'Client handshake failed.',
+            ], 3)
+            return
         # File transfer, server-side, Send file to 1 client
-        pass
+        seq_base = 0
+        current_retry = 0
+        seq_len = ceil(self.file_size / payload_length)
+        Logger.log([
+            f'[!] [File Transfer] [{client_addr}] Begin send file to client',
+            f'({seq_len} segments).',
+        ], 3)
+        # Send metadata
+        self.conn.send_data(
+            Segment(
+                flags=SegmentFlag(meta=True),
+                payload=Metadata(
+                    self.file_size,
+                    seq_len,
+                    self.path_file.name,
+                ).to_bytes(),
+            ),
+            client_addr,
+        )
+        Logger.log(
+            f'[!] [File Transfer] [{client_addr}] Sent metadata to client.',
+            2,
+        )
+        # Send file
+        with open(self.path_file, 'rb') as file:
+            while seq_base < seq_len:  # loop for entire length segment
+                seq_max = min(seq_base + WINDOW_SIZE, seq_len)
+                for seq in range(seq_base, seq_max):
+                    self.__send_file_segment(file, seq, client_addr)
+                while seq_base < seq_len:  # loop for all ack
+                    # Wait for ACK
+                    data = self.conn.listen_single_segment(1, client_addr)
+                    # if timeout, go back n
+                    if data is None:
+                        current_retry += 1
+                        # we limit go back n to max_tries times
+                        if current_retry >= max_retry:
+                            Logger.log([
+                                f'[!] [File Transfer] [{client_addr}] Max',
+                                'retry reached, assuming connection closed.',
+                            ], 3)
+                            return
+                        Logger.log([
+                            f'[Segment SEQ={seq_base}] NOT ACKED.',
+                            'Timeout.',
+                        ], 1)
+                        break
+                    # data is available
+                    # reset tries counter
+                    current_retry = 0
+                    # read data
+                    segment, addr = data
+                    if all([
+                        segment.flags.test(ack=True),
+                        segment.ack_num == seq_base,
+                    ]):
+                        Logger.log([
+                            f'[Segment SEQ={segment.ack_num}] [{addr}]',
+                            'Acked.',
+                        ], 2)
+                        seq_base += 1
+                        # Send next seq if available
+                        if seq_base + WINDOW_SIZE - 1 < seq_len:
+                            self.__send_file_segment(
+                                file,
+                                seq_base + WINDOW_SIZE - 1,
+                                client_addr,
+                            )
+                    else:
+                        Logger.log([
+                            f'[Segment SEQ={segment.ack_num}] [{addr}] NOT',
+                            f'ACKED. Different ack num than {seq_base}.',
+                            'Ignored.',
+                        ], 1)
+        # Tear down connection
+        Logger.log([
+            f'[!] [File Transfer] [{client_addr}] File transfer',
+            'completed!',
+        ], 3)
+        self.conn.close_socket(client_addr)
+        Logger.log([
+            f'[!] [Handshake] [?{client_addr}]',
+            f'Waiting for FIN from {client_addr}.',
+        ], 1)
+        data = self.conn.listen_single_segment(handshake_timeout, client_addr)
+        if data is None:
+            Logger.log([
+                f'[!] [Handshake] [{client_addr}] FIN not received.',
+                'Assuming connection closed.',
+            ], 3)
+        else:
+            Logger.log(f'[!] [x{client_addr}] Connection closed!', 3)
 
     def three_way_handshake(self, client_addr: Address) -> bool:
-       # Three way handshake, server-side, 1 client
-        print(
-            '[!] [Handshake|SYN] Sending SYN to client with address:',
-            f'{client_addr.ip}:{client_addr.port}',
-        )
+        # Three way handshake, server-side, 1 client
+        Logger.log([
+            f'[!] [Handshake|SYN] [>{client_addr}]',
+            'Sending SYN to client.',
+        ], 2)
         self.conn.send_data(
             Segment(flags=SegmentFlag(syn=True)),
             client_addr,
         )
-        print('[!] [Handshake] Waiting for client SYN-ACK...')
-        segment, addr = self.conn.listen_single_segment()
+        Logger.log([
+            f'[!] [Handshake] [?{client_addr}]',
+            'Waiting for client SYN-ACK...',
+        ], 1)
+        data = self.conn.listen_single_segment(handshake_timeout, client_addr)
+        if data is None:
+            Logger.log([
+                f'[!] [Handshake] [{client_addr}]',
+                'SYN-ACK timeout, assuming connection closed.',
+            ], 1)
+            return False
+        segment = data[0]
         if segment.flags.test(syn=True, ack=True):
-            print(
-                '[!] [Handshake|SYN-ACK] Receive SYN-ACK',
-                'from client with address:',
-                f'{addr.ip}:{addr.port}',
+            Logger.log(
+                f'[!] [Handshake|SYN-ACK] [<{client_addr}] Received SYN-ACK.',
+                2,
             )
-        print('[!] [Handshake|ACK] Sending ACK to the client...')
         self.conn.send_data(
             Segment(flags=SegmentFlag(ack=True)),
             client_addr,
         )
-        print('[!] [Handshake] Connection established!')
+        Logger.log(
+            f'[!] [Handshake|ACK] [>{client_addr}] Sent ACK to the client.',
+            2,
+        )
+        Logger.log(
+            f'[!] [Handshake] [{client_addr}] Connection established!',
+            2,
+        )
+        return True
 
 
 if __name__ == '__main__':
@@ -73,10 +231,29 @@ if __name__ == '__main__':
         type=int,
         help='Broadcast port. Default is 1234.',
     )
+    args_parser.add_argument(
+        'path_file_input',
+        action=FileValidator,
+        help='Path for input file to send into the clients.',
+    )
+    args_parser.add_argument(
+        '-v',
+        '--verbose',
+        action='count',
+        default=0,
+        help='Verbosity level.',
+    )
+    args_parser.add_argument(
+        '-p',
+        '--parallel',
+        action='store_true',
+        help='Send file to all clients in parallel.',
+    )
     args = args_parser.parse_args()
     main = Server(
         args.path_file_input,
         port=args.broadcast_port,
+        parallel=args.parallel,
     )
+    Logger.level = args.verbose
     main.listen_for_clients()
-    main.start_file_transfer()
